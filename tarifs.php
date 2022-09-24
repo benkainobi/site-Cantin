3092f896d7083bf7c4504ebecdac85bbf297c98e
benjamin kai <benjaminkai@benjamins-MacBook-Air.local>
2022-09-24 13:43:08 +0200

    page tarifs et css de la page

--- tarifs.php
+++ tarifs.php
@@ -1,5 +1,23 @@
 
 <?php include "barre_menu.php" ?>
-tarifs  
+<div class="fond">
+    <div class="boitetrans">
+<div class="titre"><h1>Tarifs et prestations :</h1></div>
+<div class="titre"><h2>« Juste moi » 130 euros</h2> </div>
+<div class="titre"><h4>Séance pour une personne, en extérieur ou en studio</h4></div>
+<div class="titre"><h2> Pour deux » 195 euros«</h2></div>
+<div class="titre"><h4>Pour deux personnes, en extérieur ou en studio</h4></div>
+<div class="titre"><h2> « Famille » 220 euros</h2></div>
+<div class="titre"><h4><div style="text-align:center">Pour la famille ou les amis jusqu’à 4 personnes, en extérieur ou en studio</div>
+<div style="text-align:center">30 euros en supplément par personne au-delà de 4 (hormis enfant jusqu’à 2 ans)</div></br>
+<div class="titre"><h2> « Il était une fois » 160 euros</h2></div>
+<div class="titre"><h4> Photo de grossesse (À votre domicile, en extérieur ou en studio)</h4></div>
+<div class="titre"> <h2>« Mon bébé » 100 euros</h2></div>
+<div class="titre"><h4> Photo d’enfant jusqu’à 3 ans (photo à domicile)</h4></div>
+<div class="titre"> <h2>« J’immortalise l’événement » </div>
+<div class="titre"><h4> sur mesure Prestation de mariage ou baptême sur devis</h4></div>
+<div class="titre"> <h6>ECF d’entraînement Développer la partie front-end d’une application web</h6></div>
+</div>
+</div>
 </body>
 </html>
