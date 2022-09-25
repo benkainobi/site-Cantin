18512e353597d5ad544c698ff105a57ad5628283
benjamin kai <benjaminkai@benjamins-MacBook-Air.local>
2022-09-25 11:24:00 +0200

    mise en place instagram, facebook,twitter

--- barre_menu.php
+++ barre_menu.php
@@ -7,9 +7,12 @@
 </head>
 <body>
 
-<ul>
-    <li><a href="accueil.php">Accueil</a></li>
-    <li><a href="galerie.php">Galerie</a></li>
-    <li><a href="tarifs.php">Tarifs</a></li>
-    <li><a href="contact.php">Contact</a></li>
+<ul class="ul">
+    <li class="li"><a href="accueil.php">Accueil</a></li>
+    <li class="li"><a href="galerie.php">Galerie</a></li>
+    <li class="li"><a href="tarifs.php">Tarifs</a></li>
+    <li class="li"><a href="contact.php">Contact</a></li>
+    <li class="logo"><a href="http://twitter.com"><img src="twitter.png" ></a><li>
+    <li class="logo"><a href="http://facebook.com"><img src="facebook.png" ></a></li>
+    <li class="logo"><a href="http://instagram.com"><img src="instagram.png" ></a></li>
 </ul>
